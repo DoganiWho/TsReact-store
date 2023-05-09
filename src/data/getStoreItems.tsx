a8248cf27ab0938589c7538b69e936dc0c11b8ab
DoganiWho <96683644+DoganiWho@users.noreply.github.com>
2023-05-09 12:26:03 +0000

    started Store page

--- src/data/getStoreItems.tsx
+++ src/data/getStoreItems.tsx
@@ -1,7 +1,8 @@
 async function getStoreItems() {
-  fetch('https://fakestoreapi.com/products/1')
+  const data = await fetch('https://fakestoreapi.com/products/1')
     .then(res => res.json())
     .then((json) => console.log(json));
+  return data
 }
 
 export default getStoreItems;
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,6 +1,6 @@
 import { Row, Col } from "react-bootstrap"
 import { StoreItem } from "../components/StoreItem"
-import getStoreItems from "../data/getStoreItems"
+// import getStoreItems from "../data/getStoreItems"
 {/* TODO: 
   * get store items from fakestore-api
   * Work on Store page
@@ -15,11 +15,11 @@ const storeItems = getStoreItems();
     <>
       <h1>Store</h1>
       <Row md={2} xs={1} lg={3} className="g-3">
-        {storeItems.map(item => {
+        {/* {storeItems.map(item => {
           <Col key={item.id}>
             <StoreItem {...item} />
           </Col>
-        })}
+        })} */}
       </Row>
     </>
   );
